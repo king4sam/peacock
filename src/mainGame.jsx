eae6d94937cac39d6288f1cd17646bdcc0e3df88
鄭育承 <king4sam990816@gmail.com>
2019-09-25 23:55:53 +0800

    refactor: deconstructing

--- src/mainGame.jsx
+++ src/mainGame.jsx
@@ -8,35 +8,33 @@ class MainGame extends React.Component {
     super(props);
     this.sendData = this.sendData.bind(this);
     this.state = { msgArray: [lineMap.get(1)] };
+    this.render = this.render.bind(this);
   }
 
   sendData(e, choice) {
-    // e.preventDefault()
-    // e.persist()
+    const {
+      state: { msgArray },
+    } = this;
     console.log('choice', choice);
     console.log(lineMap.get(choice));
     console.log('state', this.state);
     console.log('update ary ', {
-      ...this.state.msgArray[this.state.msgArray.length - 1],
+      ...msgArray[msgArray.length - 1],
       questionStatus: questionStatus.continue,
     });
     console.log(
       'new ',
-      lineMap.get(
-        this.state.msgArray[this.state.msgArray.length - 1].choices[choice]
-      )
+      lineMap.get(msgArray[msgArray.length - 1].choices[choice])
     );
     this.setState(
       {
         msgArray: [
-          ...this.state.msgArray.slice(0, this.state.msgArray.length - 1),
+          ...msgArray.slice(0, msgArray.length - 1),
           {
-            ...this.state.msgArray[this.state.msgArray.length - 1],
+            ...msgArray[msgArray.length - 1],
             questionStatus: questionStatus.continue,
           },
-          lineMap.get(
-            this.state.msgArray[this.state.msgArray.length - 1].choices[choice]
-          ),
+          lineMap.get(msgArray[msgArray.length - 1].choices[choice]),
         ],
       },
       () => {
@@ -50,21 +48,25 @@ class MainGame extends React.Component {
       <div>
         {messages.map(ele => (
           <GameMessage
-            key={ele.question + '123fua'}
+            key={`${ele.question}'123fua1`}
             handler={this.sendData}
             question={ele}
-          ></GameMessage>
+          />
         ))}
       </div>
     );
   }
 
   render() {
+    const {
+      state: { msgArray },
+    } = this;
+    console.log(msgArray);
     return (
       <div>
         <p className="gameTitle">To Peacock</p>
 
-        {this.renderAry(this.state.msgArray)}
+        {this.renderAry(msgArray)}
       </div>
     );
   }
