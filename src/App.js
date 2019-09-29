3e7a9cb65e05fdb69bcdfac3bdb2fd27dc1523cd
鄭育承 <king4sam990816@gmail.com>
2019-09-30 00:29:25 +0800

    use pure function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
 import Guide from './guide';
 import MainGame from './mainGame';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div className="peacockApp">
-          <Route path="/" exact component={Guide} />
-          <Route path="/guideOne" exact component={Guide} />
-          <Route path="/mainGame/" component={MainGame} />
-          <Route path="/outCome/" component={MainGame} />
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Router>
+      <div className="peacockApp">
+        <Route path="/" exact component={Guide} />
+        <Route path="/guideOne" exact component={Guide} />
+        <Route path="/mainGame/" component={MainGame} />
+        <Route path="/outCome/" component={MainGame} />
+      </div>
+    </Router>
+  );
+};
 
 export default App;
